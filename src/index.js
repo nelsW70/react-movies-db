13a60dbea9f3ace252cc47fba5690e25d1d9ce5e
Nelson Foltz <nelsfoltz@gmail.com>
2020-11-15 10:28:29 -0500

    add: api key and env var

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { AppProvider } from './context'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { AppProvider } from './context';
+import { BrowserRouter as Router } from 'react-router-dom';
 ReactDOM.render(
-  <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </React.StrictMode>,
+  <AppProvider>
+    <App />
+  </AppProvider>,
   document.getElementById('root')
-)
+);
